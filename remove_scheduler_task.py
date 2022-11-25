ac32f50fedb55438a5a6e558b595d2c537d0048f
Stian <79229317+0xStian@users.noreply.github.com>
2022-11-25 23:44:50 +0100

    Update remove_scheduler_task.py

--- remove_scheduler_task.py
+++ remove_scheduler_task.py
@@ -1,10 +1,10 @@
 import os, ctypes, sys, time
 
-task_name = "task name" # name of task
-time_between_removal = 600 # time to sleep between removal
+task_name = "task name"
+time_between_removal = 600 
 
 
-def is_admin(): # checks if admin, if not puts up prompt asking for admin rights
+def is_admin(): # admin rights needed for scheduler task removal
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
@@ -13,7 +13,7 @@ def is_admin(): # checks if admin, if not puts up prompt asking for admin rights
     
 if is_admin():
     while True:
-            os.system(f"SchTasks /DELETE /F /TN {task_name}") # deletes schedular task with cmd command
+            os.system(f"SchTasks /DELETE /F /TN {task_name}")
             time.sleep(time_between_removal)
 else:
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
