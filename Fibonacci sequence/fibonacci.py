0853ba6e95bdf9ed12dffa78307dcd73c1bdd450
0xStian <stian02@pm.me>
2022-11-29 12:51:50 +0100

    fibonacci

---
+++ Fibonacci sequence/fibonacci.py
@@ -0,0 +1,14 @@
+first = 0
+second = 1
+i = 0
+length = int(input('Enter sequence lenght: '))
+my_list = []
+
+while i < length:
+    my_list.append(first)
+    my_list.append(second)
+    first = first + second
+    second = first + second
+    i += 1
+
+print(*my_list, sep=', ')
