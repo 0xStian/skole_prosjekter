217f835f85f65d3d4c30323c84a089b16603e340
0xStian <stian02@pm.me>
2023-01-03 13:19:39 +0100

    changes

---
+++ Module_5/5.3/activity1/folderScanner.py
@@ -0,0 +1,6 @@
+import hashlib, os
+
+folder_to_scan = r"C:\Users\StiMik56694\Desktop\skole_prosjekter\skole_prosjekter\Module_5\5.3\activity1\images" #input("Folder to scan: ") # C:\Users\StiMik56694\Desktop\skole_prosjekter\skole_prosjekter\Module_5\5.3\activity1\images
+path_to_save = r"C:\Users\StiMik56694\Desktop\skole_prosjekter\skole_prosjekter\Module_5\5.3\activity1" #input("Path to save: ") # C:\Users\StiMik56694\Desktop\skole_prosjekter\skole_prosjekter\Module_5\5.3\activity1
+
+
